3c67c43fcbf9a3db8b4fa4cd5ee53496a177d7ee
Mikel Larrañaga <mikel.larranaga@ehu.eus>
2016-05-16 11:53:48 +0200

    Hasierako bertsioa

---
+++ C3/inst/htmlwidgets/C3Gauge.js
@@ -0,0 +1,51 @@
+HTMLWidgets.widget({
+
+  name: 'C3Gauge',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+  renderValue: function(x) {
+        // create a chart and set options
+        // note that via the c3.js API we bind the chart to the element with id equal to chart1
+        var chart = c3.generate({
+            bindto: el,
+            data: {
+                json: x,
+                type: 'gauge'
+            },
+            gauge: {
+                label:{
+                    //returning here the value and not the ratio
+                    format: function(value, ratio){ return value;}
+                },
+                min: 0,
+                max: 1,
+                width: 15,
+                units: 'value' //this is only the text for the label
+            },
+	          color:{
+	            pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three color levels for the percentage values.
+              threshold: {
+              // unit: 'value', // percentage is default
+              // max: 200, // 100 is default
+            values: [.20, .40, .60, .80]
+        }
+	  }
+        });
+              
+      },
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
